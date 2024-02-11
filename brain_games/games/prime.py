c27471e60512535adedbbf7e5f298edc252a984d
Malik Nizamov <malik.nizamovv@gmail.com>
2024-02-11 16:02:10 +0300

    add PRIME

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,7 +2,7 @@ import random
 import prompt
 
 
-max_round = 3
+max_rounds = 3
 
 
 def prime_number():
@@ -10,8 +10,8 @@ def prime_number():
     name = prompt.string('May I have your name? ').capitalize()
     print(f'Hello, {name}!')
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
-    round = 0
-    while round != max_round:
+    rounds = 0
+    while rounds < max_rounds:
         number = random.randint(2, 100)
         print(f'Question: {number}')
         lts = []
@@ -26,9 +26,9 @@ def prime_number():
         if user_answer == right_answer:
             print('Correct')
         else:
-            print(f"""{user_answer} is wrong answer ;(.
-Correct answer was {right_answer}. Let's try again, {name}!""")
-            break
-        round += 1
-    if round == max_round:
-        print(f'Congratulations, {name}!')
+            print(f'{user_answer} is wrong answer ;(.')
+            print(f'Correct answer was {right_answer}.')
+            print(f"Let's try again, {name}!")
+            return
+        rounds += 1
+    print(f'Congratulations, {name}!')
