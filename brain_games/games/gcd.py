b236b3809845b38f7af0d8d1613488a3109340bd
Malik Nizamov <malik.nizamovv@gmail.com>
2024-03-19 19:36:14 +0300

    add gcd

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,33 +1,21 @@
+from brain_games.games.general_logging import logging
 from random import randint
-import prompt
 
 
-MAX_ROUNDS = 3
+tasks = 'Find the greatest common divisor of given numbers.'
 
 
-def divisor():
-    round = 0
-    print('Welcome to the Brain Games!')
-    name = prompt.string('May i have your name? ').capitalize()
-    print(f'Hello, {name}!')
-    print('Find the greatest common divisor of given numbers.')
-    while round != MAX_ROUNDS:
-        number_one = randint(1, 100)
-        number_two = randint(1, 100)
-        print(f'Question: {number_one} {number_two}')
-        your_answer = prompt.integer('Your answer: ')
-        while number_one != number_two:
-            if number_one > number_two:
-                number_one -= number_two
-            else:
-                number_two -= number_one
-        if number_one == your_answer:
-            print('Correct!')
+def gcd():
+    number_one = randint(1, 20)
+    number_two = randint(1, 20)
+    question = f'{number_one} {number_two}'
+    while number_one != number_two:
+        if number_one > number_two:
+            number_one -= number_two
         else:
-            print(f"""
-{your_answer} is wrong answer,;(. Correct answer was '{number_one}'
-Let's try again, {name}!""")
-            break
-        round += 1
-    if round == MAX_ROUNDS:
-        print(f'Congratulations, {name}!')
+            number_two -= number_one
+    return question, str(number_one)
+
+
+def play():
+    logging(game=gcd, text=tasks)
