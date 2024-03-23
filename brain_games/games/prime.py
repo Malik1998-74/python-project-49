b4e74907613733dd7fbb4937c43d3bcaad073933
Malik Nizamov <malik.nizamovv@gmail.com>
2024-03-23 12:40:39 +0300

    add prime

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,21 +2,19 @@ from brain_games.games.general_logging import logging
 import random
 
 
-tasks = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+WELCOME_TEXT_PRIME: str = 'Answer "yes" if given number is prime.' \
+    'Otherwise answer "no".'
 
 
 def prime():
-    number = random.randint(2, 100)
-    lts = []
-    for char in range(2, number + 1):
-        if number % char == 0:
-            lts.append(char)
+    number: int = random.randint(2, 100)
+    lts: list = [char for char in range(2, number + 1) if not number % char]
     if len(lts) == 1:
-        right_answer = 'yes'
+        right_answer: str = 'yes'
     else:
-        right_answer = 'no'
+        right_answer: str = 'no'
     return str(number), right_answer
 
 
 def play():
-    logging(game=prime, text=tasks)
+    logging(game=prime, text=WELCOME_TEXT_PRIME)
