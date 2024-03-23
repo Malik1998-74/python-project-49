a77fce3d1062f06b2257ce0236efeb1100abf7f1
Malik Nizamov <malik.nizamovv@gmail.com>
2024-03-23 12:39:25 +0300

    add calc

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -2,23 +2,23 @@ from brain_games.games.general_logging import logging
 import random
 
 
-tasks = 'What is the result of the expression?'
-symbols = ['+', '-', '*']
+WELCOME_TEXT_CALC: str = 'What is the result of the expression?'
+symbols: list = ['+', '-', '*']
 
 
 def calc():
     symbol = random.choice(symbols)
-    number_one = random.randint(1, 10)
-    number_two = random.randint(1, 10)
-    question = f'{number_one} {symbol} {number_two}'
+    number_one: int = random.randint(1, 10)
+    number_two: int = random.randint(1, 10)
+    question: str = f'{number_one} {symbol} {number_two}'
     if symbol == '+':
-        right_answer = number_one + number_two
+        right_answer: int = number_one + number_two
     elif symbol == '-':
-        right_answer = number_one - number_two
+        right_answer: int = number_one - number_two
     else:
-        right_answer = number_one * number_two
+        right_answer: int = number_one * number_two
     return question, str(right_answer)
 
 
 def play():
-    logging(game=calc, text=tasks)
+    logging(game=calc, text=WELCOME_TEXT_CALC)
