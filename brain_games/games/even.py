4d330c85b830c69b52d4b5e64bd297baa3e9cdda
Malik Nizamov <malik.nizamovv@gmail.com>
2024-03-23 12:39:36 +0300

    add even

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -2,17 +2,18 @@ from brain_games.games.general_logging import logging
 from random import randint
 
 
-tasks = 'Answer "yes" if the number is even, otherwise answer "no".'
+WELCOME_TEXT_EVEN: str = 'Answer "yes" if the number is even,' \
+    'otherwise answer "no".'
 
 
 def even():
-    number = randint(0, 10)
+    number: int = randint(0, 10)
     if number % 2 == 0:
-        right_answer = 'yes'
+        right_answer: str = 'yes'
     else:
-        right_answer = 'no'
+        right_answer: str = 'no'
     return str(number), right_answer
 
 
 def play():
-    logging(game=even, text=tasks)
+    logging(game=even, text=WELCOME_TEXT_EVEN)
